29eb46ff19b651eaf6b821d48400896baea4b773
Luiz Eduardo F <luizeduardo_francodarocha@outlook.com>
2023-08-19 18:25:13 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEBMVC.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(35)]
+        public string nome { get; set; }
+
+        [StringLength(30)]
+        public string disciplina { get; set; }
+
+        public float nota1 { get; set; }
+        public float nota2 { get; set; }
+    }
+}
---
+++ Models/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WEBMVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options): base(options) {}
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
